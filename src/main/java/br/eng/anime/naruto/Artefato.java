35e472521851c3076ad7a58ac26b058cbc550d5e
JoaoPedro-br <jpcsoares1@gmail.com>
2025-09-01 17:39:30 -0300

    Troca de user.name da commit (ambos os anteriores foi por JoaoPedro-br, apenas havia esquecido de trocar o user.name desse projeto

--- src/main/java/br/eng/anime/naruto/Artefato.java
+++ src/main/java/br/eng/anime/naruto/Artefato.java
@@ -1,5 +1,15 @@
 package br.eng.anime.naruto;
 
 public class Artefato {
+    private String nome;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
 }
